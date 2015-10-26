7cea4bcbb4acbaeaec20b68f0f609bd5d0442b93
Frederik Spang <frederik@frederikspang.dk>
2015-10-26 20:47:58 +0100

    Add support for multienvironment config

--- wp-config.php
+++ wp-config.php
@@ -3,9 +3,9 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, and ABSPATH. You can find more information by visiting
- * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
- * Codex page. You can get the MySQL settings from your web host.
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
@@ -14,96 +14,55 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+// Try environment variable 'WP_ENV'
+if (getenv('WP_ENV') !== false) {
+    // Filter non-alphabetical characters for security
+    define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
+}
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+// Define site host
+if (isset($_SERVER['X_FORWARDED_HOST']) && !empty($_SERVER['X_FORWARDED_HOST'])) {
+    $hostname = $_SERVER['X_FORWARDED_HOST'];
+} else {
+    $hostname = $_SERVER['HTTP_HOST'];
+}
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+// Try server hostname
+if (!defined('WP_ENV')) {
+    // Set environment based on hostname
+    include 'config/wp-config.env.php';
+}
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+// Are we in SSL mode?
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+    $protocol = 'https://';
+} else {
+    $protocol = 'http://';
+}
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+// Load default config
+include 'config/wp-config.default.php';
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/wordpress/');
+// Load config file for current environment
+include 'config/wp-config.' . WP_ENV . '.php';
 
-define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST']);
-define('WP_CONTENT_DIR', realpath(ABSPATH . '../wp-content/'));
-define('WP_CONTENT_URL', WP_HOME . '/wp-content');
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
-define('UPLOADS', '../uploads');
+// Define WordPress Site URLs if not already set in config files
+if (!defined('WP_SITEURL')) {
+    define('WP_SITEURL', $protocol . rtrim($hostname, '/'));
+}
+if (!defined('WP_HOME')) {
+    define('WP_HOME', $protocol . rtrim($hostname, '/'));
+}
 
-/**
- * Starting with Wordpress 3.7, minor and security updates are rolled out automatically. Detailed information can be
- * found here: http://make.wordpress.org/core/2013/10/25/the-definitive-guide-to-disabling-auto-updates-in-wordpress-3-7/
- *
- * Depening on the project, environment and deployment strategy it might not be ideal to allow Wordpress to do automatic
- * updates. Possible options are:
- * 1. Checkout the project from a repository. Wordpress will never auto-update.
- * 2. Don't allow file modifications through the admin; config: DISALLOW_FILE_MODS
- * 3. Don't allow automatic updates; config: AUTOMATIC_UPDATER_DISABLED
- * 4. Only disable core updates; config: WP_AUTO_UPDATE_CORE
- *
- * Basically only option one and two are relevant for automatically deployed environments.
- */
-//define('DISALLOW_FILE_MODS', true);
-//define('AUTOMATIC_UPDATER_DISABLED', true);
-//define('WP_AUTO_UPDATE_CORE', true);    // major and minor updates
-//define('WP_AUTO_UPDATE_CORE', false);   // no updates at all
-//define('WP_AUTO_UPDATE_CORE', 'minor'); // only minor updates
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
+// Clean up
+unset($hostname, $protocol);
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/wordpress/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-config/wp-config.test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Development environment config settings
+ *
+ * Enter any WordPress config settings that are specific to this environment
+ * in this file.
+ *
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    1.0
+ * @author     Studio 24 Ltd  <info@studio24.net>
+ */
+
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('WP_HOME', 'http://localhost');
+define('WP_SITEURL', 'http://localhost/wordpress');
+define('WP_CONTENT_DIR', realpath(ABSPATH . '../wp-content/'));
+define('WP_CONTENT_URL', WP_HOME . '/wp-content');
+
+define('DB_NAME', 'database_name_here');
+define('DB_USER', 'username_here');
+define('DB_PASSWORD', 'password_here');
+define('DB_HOST', 'localhost');
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', true);
